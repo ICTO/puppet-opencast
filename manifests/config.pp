6eab8220f42110e7c319742cc80967ad12ada4a4
Kristof Keppens <kkeppens@gmail.com>
2012-12-05 08:33:45 +0100

    add config for segmenting, workflow and usertracking

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,25 +11,29 @@ class opencast::config {
 
   Class['opencast::params'] -> Class['opencast::config']
 
-  $opencast_profile = $opencast::params::opencast_profile
-  $opencast_server_url = $opencast::params::opencast_server_url
-  $opencast_admin_url = $opencast::params::opencast_admin_url
-  $opencast_engage_url = $opencast::params::opencast_engage_url
-  $opencast_storage_dir = $opencast::params::opencast_storage_dir
-  $opencast_digest_user_name = $opencast::params::opencast_digest_user_name
-  $opencast_digest_user_pass = $opencast::params::opencast_digest_user_pass
-  $opencast_enable_demo_user = $opencast::params::opencast_enable_demo_user
-  $opencast_streaming_url = $opencast::params::opencast_streaming_url
-  $opencast_db_vendor = $opencast::params::opencast_db_vendor
-  $opencast_db_driver = $opencast::params::opencast_db_driver
-  $opencast_db_url = $opencast::params::opencast_db_url
-  $opencast_db_user = $opencast::params::opencast_db_user
-  $opencast_db_pass = $opencast::params::opencast_db_pass
-  $opencast_ffmpeg_path = $opencast::params::opencast_ffmpeg_path
-  $opencast_mediainfo_path = $opencast::params::opencast_mediainfo_path
-  $opencast_tesseract_path = $opencast::params::opencast_tesseract_path
-  $opencast_gst_launch_path = $opencast::params::opencast_gst_launch_path
-  $opencast_admin_email = $opencast::params::opencast_admin_email
+  $opencast_profile           = $opencast::params::opencast_profile
+  $opencast_server_url        = $opencast::params::opencast_server_url
+  $opencast_admin_url         = $opencast::params::opencast_admin_url
+  $opencast_engage_url        = $opencast::params::opencast_engage_url
+  $opencast_storage_dir       = $opencast::params::opencast_storage_dir
+  $opencast_digest_user_name  = $opencast::params::opencast_digest_user_name
+  $opencast_digest_user_pass  = $opencast::params::opencast_digest_user_pass
+  $opencast_enable_demo_user  = $opencast::params::opencast_enable_demo_user
+  $opencast_streaming_url     = $opencast::params::opencast_streaming_url
+  $opencast_db_vendor         = $opencast::params::opencast_db_vendor
+  $opencast_db_driver         = $opencast::params::opencast_db_driver
+  $opencast_db_url            = $opencast::params::opencast_db_url
+  $opencast_db_user           = $opencast::params::opencast_db_user
+  $opencast_db_pass           = $opencast::params::opencast_db_pass
+  $opencast_ffmpeg_path       = $opencast::params::opencast_ffmpeg_path
+  $opencast_mediainfo_path    = $opencast::params::opencast_mediainfo_path
+  $opencast_tesseract_path    = $opencast::params::opencast_tesseract_path
+  $opencast_gst_launch_path   = $opencast::params::opencast_gst_launch_path
+  $opencast_admin_email       = $opencast::params::opencast_admin_email
+  $stabilitythreshold         = $opencast::params::stabilitythreshold
+  $changesthreshold           = $opencast::params::changesthreshold
+  $usertracking_enable        = $opencast::params::usertracking_enable
+  $max_concurrent             = $opencast::params::max_concurrent
 
   file {"${opencast::params::configdir}/config.properties":
     ensure  => present,
@@ -45,5 +49,35 @@ class opencast::config {
     group   => $opencast::params::group,
     mode    => '0644',
     content => template('opencast/matterhorn.conf.erb');
+  }
+
+  if $opencast::params::config_segmenting {
+    file {"${opencast::params::configdir}/services/org.opencastproject.analysis.vsegmenter.VideoSegmenter.properties":
+      ensure  => file,
+      user    => $opencast::params::user,
+      group   => $opencast::params::group,
+      mode    => 0644,
+      content => template('opencast/services/org.opencastproject.analysis.vsegmenter.VideoSegmenter.properties.erb');
+    }
+  }
+
+  if $usertracking_enable != undef {
+    file {"${opencast::params::configdir}/services/org.opencastproject.usertracking.impl.UserTrackingServiceImpl.properties":
+      ensure  => file,
+      user    => $opencast::params::user,
+      group   => $opencast::params::group,
+      mode    => 0644,
+      content => template('opencast/services/org.opencastproject.usertracking.impl.UserTrackingServiceImpl.properties.erb');
+    }
+  }
+
+  if $max_concurrent != undef {
+    file {"${opencast::params::configdir}/services/org.opencastproject.workflow.impl.WorkflowServiceImpl.properties":
+      ensure  => file,
+      user    => $opencast::params::user,
+      group   => $opencast::params::group,
+      mode    => 0644,
+      content => template('opencast/services/org.opencastproject.workflow.impl.WorkflowServiceImpl.properties.erb');
+    }
   }
 }
