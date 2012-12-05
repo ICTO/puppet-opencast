3ca9ef32dbb2f87c14bc16103b9d82b4b22cf126
Kristof Keppens <kkeppens@gmail.com>
2012-12-05 08:46:48 +0100

    add support for gstreamer encoding profiles

--- manifests/encodings.pp
+++ manifests/encodings.pp
@@ -1,13 +1,28 @@
 define opencast::encoding (
   $encoding = $title,
-  $content
+  $content,
+  $type = 'ffmpeg'
 ) {
 
-  file {"${opencast::params::configdir}/encoding/${encoding}.properties":
-    ensure  => file,
-    user    => $opencast::params::user,
-    group   => $opencast::params::group,
-    mode    => 0644,
-    content => $content;
+  case $type {
+    ffmpeg: {
+      file {"${opencast::params::configdir}/encoding/${encoding}.properties":
+        ensure  => file,
+        user    => $opencast::params::user,
+        group   => $opencast::params::group,
+        mode    => 0644,
+        content => $content;
+      }
+    }
+    gstreamer: {
+      file {"${opencast::params::configdir}/gsencoding/${encoding}.properties":
+        ensure  => file,
+        user    => $opencast::params::user,
+        group   => $opencast::params::group,
+        mode    => 0644,
+        content => $content;
+      }
+    }
+    default: { fail("Please specify the encoding type, valid types are ffmpeg or gstreamer")}
   }
 }
