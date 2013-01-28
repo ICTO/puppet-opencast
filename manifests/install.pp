3e09caac4b154e3ada971e4fe344f43167ccbb9c
Kristof Keppens <kkeppens@gmail.com>
2013-01-28 13:58:07 +0100

    install dependencies

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,4 +9,20 @@ class opencast::install {
     ensure => installed;
   }
 
+  if $::opencast::profile == 'worker' {
+    package {
+      'ffmpeg':
+        ensure => installed;
+      'mediainfo':
+        ensure => installed;
+      'tesseract-ocr':
+        ensure => installed;
+      'tesseract-ocr-eng':
+        ensure => installed;
+      'tesseract-ocr-nld':
+        ensure => installed;
+      'libgstreamer0.10':
+        ensure => installed;
+    }
+  }
 }
