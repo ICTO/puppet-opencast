a8aa1b8e6c7831e94f2d6bbcf35a59bac48409fe
Kristof Keppens <kkeppens@gmail.com>
2013-06-28 09:17:58 +0200

    further lint fixing

--- manifests/init.pp
+++ manifests/init.pp
@@ -71,7 +71,7 @@ class opencast (
   $archive_dir        = "${::opencast::storage_dir}/${::opencast::archive_folder}"
   $archiveindex_dir   = "${::opencast::local_storage_dir}/${::opencast::archiveindex_folder}"
   $files_dir          = "${::opencast::storage_dir}/${::opencast::files_folder}"
-  $workspace_dir      = "${::opencast::storage_dir}/${::opencast::workspace_folder}"s
+  $workspace_dir      = "${::opencast::storage_dir}/${::opencast::workspace_folder}"
   $workflowindex_dir  = "${::opencast::local_storage_dir}/${::opencast::workflowindex_folder}"
 
   class{'opencast::install':;} ~>
--- manifests/workflow.pp
+++ manifests/workflow.pp
@@ -1,6 +1,18 @@
+# == Define: opencast::workflow
+# Define a new opencast workflow profile
+#
+# === Parameters:
+#
+# [*workflow*]
+#   title for the workflow file
+#
+# [*content*]
+#   content of the workflow file
+#
+
 define opencast::workflow (
+  $content,
   $workflow = $title,
-  $content
 ) {
 
   file {"${opencast::configdir}/workflows/${workflow}.xml":
